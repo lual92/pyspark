7377d89beac865561dc8e1e782f276d0835328de
lual92 <usuario@curso.es>
2020-03-30 15:02:38 +0200

    Ver 1.0

---
+++ pi_calculator.py
@@ -0,0 +1,17 @@
+def pi(N):
+    import random
+    result = 0
+    for i in range(N):
+        x = random.random()
+        y = random.random()
+        r = (x ** 2 + y ** 2)
+        if r <= 1:
+            result += 1
+    return (result / N) * 4
+
+
+print(pi(10000))
+
+
+def culocaca():
+    pass
